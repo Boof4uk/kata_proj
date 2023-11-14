1b5d049a25f96d228459547d48b36b26a6cb0227
vladlove28 <sergcechisto@gmail.com>
2023-11-14 19:18:34 +0300

    antifraid-branch - базовая структура проекта

--- antifraud/src/main/java/com/bank/antifraud/serviceImpl/AntifraudAuditServiceImpl.java
+++ antifraud/src/main/java/com/bank/antifraud/serviceImpl/AntifraudAuditServiceImpl.java
@@ -8,7 +8,6 @@ import com.bank.antifraud.service.AntifraudAuditService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
