00e909c4ef25bf780549f46a177ba6dc7a9dab49
lokreiiel <mtemkz@gmail.com>
2023-11-17 22:43:12 +0300

    тесты запросов в swagger

--- transfer/src/main/java/com/bank/transfer/controller/AccountTransferController.java
+++ transfer/src/main/java/com/bank/transfer/controller/AccountTransferController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,6 +29,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/account-transfer")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Validated
 public class AccountTransferController {
 
     /**
